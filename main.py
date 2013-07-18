352ce8a261ed9c903408a031cd4804aa317a33b3
Aldo Rey Balagulan <aldo_balagulan@lavabit.com>
2013-07-18 12:49:30 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#
+# Tic Tac Dota
+#
+# main.py
+#
+# The main Python file for the game. Run this to run the game.
+
+# Import important modules
+import os, random, pygame, sys
+from pygame.locals import *
+
+# Important constants
+FPS = 30 # speed of the game
+WINDOWWIDTH = 640 # width of the game window, in pixels
+WINDOWHEIGHT = 480 # height of the game window, in pixels
+
+# Color constants (R,G,B)
+GRAY = (100,100,100)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+def main():
+    """
+    The main function.
+
+    Most other function calls are called here.
+    """
+    print("Hello world!\n")
+
+if __name__ == '__main__':
+    main()
